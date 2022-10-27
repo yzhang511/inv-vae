48072a9fde9e006fd0954580271596c42e664ef7
Yizi Zhang <yz4123@columbia.edu>
2022-10-26 20:32:11 -0400

    add real experiments

--- inv_vae/utils/helpers.py
+++ inv_vae/utils/helpers.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 from torch.autograd import Variable
 
@@ -5,6 +6,8 @@ class Namespace:
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
 
+def min_max_standardize(x):
+    return (x - x.min()) / (x.max() - x.min())
 
 def to_var(x, requires_grad=False, volatile=False):
     """
@@ -15,7 +18,7 @@ def to_var(x, requires_grad=False, volatile=False):
     return Variable(x, requires_grad=requires_grad, volatile=volatile) 
 
 
-def preprocess_neural_data(data):
+def preprocess_neural_data(data_path):
     '''
     inputs:
     
@@ -24,23 +27,26 @@ def preprocess_neural_data(data):
     
     '''
 
-    tensors = data['count_network']
-    subject_traits = data['nih_cogn']
-    subject_trans = data['all_motion'].reshape(-1,1)
-    subject_rotat = data['all_rotation'].reshape(-1,1)
-    subject_motion = np.vstack([subject_trans, subject_rotat])
-
-    A_mat = np.mean(np.squeeze(tensor[18:86, 18:86,:,:]), axis=2)
+    data = np.load(data_path, allow_pickle=True).item()
+    count_networks = data['count_network']
+    traits = data['trait']
+    translations = data['translation']
+    rotations = data['rotation']
+    
+    A_mat = np.mean(count_networks, axis=0)
     A_mat = A_mat + A_mat.transpose()
-
+    
     offset = 100
     brain_networks = []
-    for i in range(tensors.shape[-1]):
-        network = np.float32(tensors[:,:,0,i] + np.transpose(tensors[:,:,0,i]))
-        np.fill_diagonal(network, np.mean(network, 0))
-        network = network[18:86, 18:86]
+    for i in range(count_networks.shape[0]):
+        network = np.float32(count_networks[i,:,:] + np.transpose(count_networks[i,:,:]))
+        np.fill_diagonal(network, 0) # exclude self-connection from consideration
         network = network.flatten() / offset
         brain_networks.append(network)
     brain_networks = np.array(brain_networks)
     
-    return brain_networks, subject_traits, subject_motion
+    avg_trans = np.ma.masked_array(translations, mask=(translations == 0)).mean(axis=1).data.reshape(-1,1)
+    avg_rotat = np.ma.masked_array(rotations, 
+                                   mask=np.logical_or(rotations == 0, np.isnan(rotations))).mean(axis=1).data.reshape(-1,1)
+    
+    return brain_networks, traits, translations, rotations, avg_trans, avg_rotat
